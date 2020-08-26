2853ec01520aa0c908836e6fe8f53c6d19d76b9c
dianest <nddgdd@gmail.com>
2020-08-26 21:01:40 +0100

    Add .gitignore and src

---
+++ src/com/netology/hw/Main.java
@@ -0,0 +1,12 @@
+package com.netology.hw;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int boardingPassPrice = 204020;
+        int rublesForMiles = 20;
+        int bonusMiles = boardingPassPrice / rublesForMiles;
+        System.out.println(bonusMiles);
+
+    }
+}
