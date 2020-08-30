82e9348c423fe648313dcff1b6c0996e9e1d6c32
dianest <nddgdd@gmail.com>
2020-08-30 21:24:49 +0100

    Add new class

---
+++ src/com/netology/hw/BonusMilesService.java
@@ -0,0 +1,8 @@
+package com.netology.hw;
+
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int rublesForMiles = 20;
+        return cost / rublesForMiles;
+    }
+}
--- src/com/netology/hw/Main.java
+++ src/com/netology/hw/Main.java
@@ -3,10 +3,9 @@ package com.netology.hw;
 public class Main {
 
     public static void main(String[] args) {
-        int boardingPassPrice = 204020;
-        int rublesForMiles = 20;
-        int bonusMiles = boardingPassPrice / rublesForMiles;
-        System.out.println(bonusMiles);
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 11_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
